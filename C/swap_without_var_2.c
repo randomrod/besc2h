9077c421d763306408830ef668fbe3cb1923ea26
Roderick Nath Basu <43243616+randomrod@users.noreply.github.com>
2018-09-19 12:31:55 +0530

    Add files via upload

---
+++ C/swap_without_var_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int num1, num2, temp;
+    printf("enter two numbers to be swapped:");	
+	scanf("%d %d", &num1, &num2);
+	num1 = num1 + num2;
+	num2 = num1 - num2;
+	num1 = num1 - num2; 
+
+	printf("first number: %d  second number: %d", num1, num2);
+}
