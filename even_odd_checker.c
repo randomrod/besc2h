03627bfa04b1bd376ae154de8ebdf474813447d0
Roderick Nath Basu <43243616+randomrod@users.noreply.github.com>
2018-09-19 11:43:41 +0530

    Delete even_odd_checker.c

--- even_odd_checker.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{printf("Enter number (integer) to be checked");
-	int num;
-	scanf("%d", &num);
-	if (num%2==0) printf("Inputted number is even");
-	else printf("Inputted number is odd");
-}
