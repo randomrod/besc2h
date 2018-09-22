341088c48415f976902e0880081364303027d9a0
Roderick Nath Basu <rodericknathbasu@gmail.com>
2018-09-22 12:39:36 +0530

    Update 3.celcius_to_fahrenheit.c

--- C/3.celcius_to_fahrenheit.c
+++ C/3.celcius_to_fahrenheit.c
@@ -2,25 +2,40 @@
 float main()
 {
 	float cel, fahr;
-	char inp;
-	printf("enter 'c' for converting from Celcius or 'f' for converting from Fahrenheit:\n");
-	scanf("%c", &inp);
-	if (inp=='c')
+	char inp, rep='y';
+	while (rep=='y')
 	{
-		printf("enter the celcius value:");
-		scanf("%f", &cel);
-		fahr=((cel*1.8)+32);
-    	printf("%f", fahr);
-	}
-	else if (inp='f')
+		printf("enter 'c' for converting from Celcius or 'f' for converting from Fahrenheit:\n");
+	    scanf("%c", &inp);
+     	if (inp=='c')
         {
-        printf("enter the fahrenheit value:");
-		scanf("%f", &fahr);
-		cel=((fahr-32)*(0.555556));
-	 	printf("%f", cel);
+        	printf("enter the celcius value:");
+		    scanf("%f", &cel);
+		    fahr=((cel*1.8)+32);
+    	    printf("%f F", fahr);
+    	    break;
+	    }
+	    else if (inp=='f')
+	    {
+	    	printf("enter the fahrenheit value:");
+		    scanf("%f", &fahr);
+		    cel=((fahr-32)*(0.555556));
+	 	    printf("%f C", cel);
+	 	    break;
+		}
+     	else
+     	{
+     		printf("input not recognised\ntry again?\n");
+     		scanf("%c", &rep);
+     	/*	if (rep=='n')
+		    {
+		    	break;
+			}
+		    else 
+		    {
+		    	rep='y';
+			}*/
+		}
 	}
-	else
-	{
-		printf("input not recognised\ntry again?");
-	}
+
 }
