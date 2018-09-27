07ab496710e9b7b1af611d8b2435540dfe41a1ab
Roderick Nath Basu <rodericknathbasu@gmail.com>
2018-09-28 03:43:14 +0530

    Create 14.integer_sum_product.c

---
+++ C/14.integer_sum_product.c
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int sumfunc(int num)
+{
+    int sum=0, rem;
+    while(num>0)
+    {
+        rem=num%10;
+        sum=sum+rem;
+        num=num/10;
+    }
+    return sum; 
+}
+
+int prodfunc(int num)
+{
+    int prod=1, rem;
+    while (num>0)
+    {
+        rem=num%10;
+        prod=prod*rem;
+        num=num/10;
+    }
+    return prod;
+}
+
+int main()
+{
+    int num1;
+    printf("enter an Integer: ");
+    scanf("%d", &num1);
+    printf("The Sum of all digits in the interger is %d\n", sumfunc(num1));
+    printf("The Product of all digits in the interger is %d\n", prodfunc(num1));
+    system("pause");
+    return 0;
+}
