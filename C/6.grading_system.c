161650596069c9da46151775d853f71db1f4328d
Roderick Nath Basu <rodericknathbasu@gmail.com>
2018-09-28 11:51:59 +0530

    Update 6.grading_system.c

--- C/6.grading_system.c
+++ C/6.grading_system.c
@@ -1,3 +1,19 @@
+/*6.      Write a program to generate the "Grade System"
+
+Marks Grade
+
+(i) Above 90 Grade "A"
+
+(ii) 81 - 90 Grade "B"
+
+(iii) 71 - 80 Grade "C"
+
+(iv) 61-70 Grade "D"
+
+(v) 51-60 Grade "E"
+
+(vi) Otherwise Grade "Fail" */
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
