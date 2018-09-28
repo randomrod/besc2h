666c970dc144d027c729b2b8067d13fffe2d0eb0
Roderick Nath Basu <rodericknathbasu@gmail.com>
2018-09-28 11:42:48 +0530

    Create 10.first_n_odd_num_add.c

---
+++ C/10.first_n_odd_num_add.c
@@ -0,0 +1,15 @@
+//10.  Write a program to compute the sum of the first n odd numbers.
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n,sum=0;
+	printf("enter the number of odd terms to be added:\n");
+	scanf("%d", &n);
+	int i=1;
+	for (i=1;i<=2*n;i=i+2)
+	{
+		sum=sum+i;
+	}
+	printf("%d", sum);
+}
