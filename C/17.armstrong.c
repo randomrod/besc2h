50be060434e87a5516c7fbec0d7dd3af2ce03f7f
Roderick Nath Basu <rodericknathbasu@gmail.com>
2018-10-20 21:08:11 +0530

    Create 17.armstrong.c

---
+++ C/17.armstrong.c
@@ -0,0 +1,36 @@
+//17. Write a program to find all Armstrong numbers within a given range.
+
+#include<stdio.h>
+#include<math.h>
+
+int armstrongchecker(int num){
+    int dig,armsum=0,temp;
+    temp=num;
+    while(num>0){
+        dig=num%10;
+        armsum=armsum+pow(dig,3);
+        num/=10; 
+    }
+    if (armsum==temp)
+    return 0;
+    else return 1;
+}
+
+long armrange(int lowbound,int upbound){
+    int i=lowbound,ret;
+    for(i=lowbound;i<=upbound;i++){
+        ret=armstrongchecker(i);
+        if (ret==0)
+        printf("%d\n", i);
+    }
+    return 0;
+}
+
+int main(){
+    int num,lowbound,upbound,temp;
+    printf("enter the lower bound and upper bound of the range :");
+    scanf("%d %d", &lowbound, &upbound);
+    printf("the armstrong numbers between %d and %d are \n", lowbound,upbound);
+    printf("%d", armrange(lowbound,upbound));
+    return  0;
+}
