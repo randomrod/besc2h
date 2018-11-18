cfb46b890d8a427d109579840b8c521f999eb2f0
Roderick Nath Basu <rodericknathbasu@gmail.com>
2018-11-18 13:04:57 +0530

    Update bubblesort.c

--- C/bubblesort.c
+++ C/bubblesort.c
@@ -1,39 +1,41 @@
-
 #include<stdio.h>
+#define size 150
 
-void bubblesort(int a[])
-{
-	int pass, j, temp, flag=0;
-	int len=5;
-	for (pass=0; pass<len-1; pass++)
-	{
-		for (j=0; j<len-pass-1; j++)
-		{
-			if (a[j]>a[j+1])
-			{
-				temp=a[j];
-				a[j]=a[j+1];
-				flag=1;
-				a[j+1]=temp;
-			}
-		}
-		if (flag==0)
-		break;
-	}
+void bubblesort(int array[], int n){
+    int pass=0,j=0,flag=0,temp;
+    for (pass=0;pass<n;pass++){
+        for (j=0;j<n-pass-1;j++){
+            if (array[j]>array[j+1]){
+                temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+                flag=1;
+            }
+        }
+        if(flag==0)
+            break;    
+    }
 }
 
-int main()
-{
-	int a[5], i, n;
-	printf("enter the numbers");
-	for(i=0; i<5; i++)
-	{
-		scanf("%d", &n);
-		a[i] = n;
-	}
-	bubblesort(a);
-    for(i=0; i<5; i++)
-	{
-		printf("%d", a[i]);
-	}
+int main(){
+    int n,i,array[size];
+    printf("enter the total number of elements to be inserted into the array: ");
+    scanf("%d", &n);
+    printf("enter the elements:\n");
+    for (i=0;i<n;i++){
+        scanf("%d", &array[i]);
+    }
+    printf("the unsorted array is: \n");
+    for (i=0;i<n;i++){
+        printf("%d ", array[i]);
+    }
+
+    bubblesort(array,n);
+    printf("\nthe sorted array is:\n");
+
+    for (i=0;i<n;i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    return 0;
 }
