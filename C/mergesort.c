bf782b7a9827af88eb64e1a2e126e78ad92d41ad
Roderick Nath Basu <rodericknathbasu@gmail.com>
2018-11-18 13:05:42 +0530

    Update mergesort.c

--- C/mergesort.c
+++ C/mergesort.c
@@ -1,70 +1,70 @@
 #include<stdio.h>
-#include<stdlib.h>
-#define size 1000
-int merge(int array[], int beg, int mid, int end)
-{
-	int i=beg,j=mid+1,index,k=0,temp[size];
-	index=0;
-	while (i<=mid && j<=end)
-	{
-		if (array[i]<array[j])
-		{
-			temp[index]=array[i];
-			i++;
-		}
-		else
-		{
-			temp[index]=array[j];
-			j++;
-		}
-		index++;
-	}
-	if (i>mid)
-	{
-		while (j<=end)
-		{
-			temp[index]=array[j];
-			j++;
-			index++;
-		}
-	}
-	else {
-		while (i<=mid)
-		{
-			temp[index]=array[i];
-			i++;
-			index++;
-		}
-	}
-	while (k<index){
-		array[k]=temp[k];
-		k++;
-	}
+#define size 150
+
+void merge(int array[],int beg,int mid,int end);
+void mergesort(int array[], int beg, int end){
+    int mid;
+    if (beg<end){
+        mid=beg+(end-beg)/2;
+        mergesort(array,beg,mid);
+        mergesort(array,mid+1,end);
+        merge(array,beg,mid,end);
+    }
+   
 }
 
-int merge_sort(int array[], int beg, int end)
-{
-	int mid;
-	if (beg<end)
-	{
-		mid=(beg+end)/2;
-		merge_sort(array, beg, mid);
-		merge_sort(array, mid+1, end);
-		merge(array, beg, mid, end);
-	}
+void merge(int array[],int beg,int mid,int end){
+    int k,temp[size],i=beg,j=mid+1, index=beg;
+    while ((i<=mid) && (j<=end)){
+        if (array[i]>=array[j]){
+            temp[index]=array[j];
+            j++;
+        }
+        else{
+            temp[index]=array[i];
+            i++;
+        }
+        index++;
+    }
+    if (i>mid){
+        while (j<=end){
+            temp[index]=array[j];
+            j++;
+            index++;
+        }
+    }
+    else{
+        while(i<=mid){
+            temp[index]=array[i];
+            i++;
+            index++;
+        }
+    }
+    for (k=beg;k<index;k++){
+        array[k]=temp[k];
+    }
 }
+
 
 int main(){
-	int i,n,array[size];
-	printf("enter the number of elements to be stored in the array:\n");
-	scanf("%d", &n);
-	printf("enter the element of the array:\n");
-	for (i=0;i<=n;i++){
-		scanf("%d", &array[i]);
-	}
-	merge_sort(array,0,n-1);
-	printf("the sorted array is:\n");
-	for (i=0; i<n;i++){
-		printf("%d\t", array[i]);
-	}
+    int n,i,array[size];
+    printf("enter the total number of elements to be inserted into the array: ");
+    scanf("%d", &n);
+    printf("enter the elements:\n");
+    for (i=0;i<n;i++){
+        scanf("%d", &array[i]);
+    }
+    printf("the unsorted array is: \n");
+    for (i=0;i<n;i++){
+        printf("%d ", array[i]);
+    }
+
+    mergesort(array,0,n-1);
+    printf("\nthe sorted array is:\n");
+
+    for (i=0;i<n;i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    return 0;
 }
